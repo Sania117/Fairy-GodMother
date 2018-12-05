646398b2a8397802365c605936f83c80c9ee0ca7
Sania117 <43829099+Sania117@users.noreply.github.com>
2018-12-05 17:10:30 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,10 @@
+$(".Carriage").hide();
+$(".Cheese").click(function() {
+    $(".Carriage").fadeIn();
+});
+$(".Gus").click(function(){
+    $(".Cinderelle").slideDown();
+});
+$(".Evil").click(function() {
+    $(".Step").toggle();
+});
